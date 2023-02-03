eaeb75259e6c3124c66c79f38912c694c614ebef
AsafNudler <me@asafnudler.com>
2023-02-03 12:20:27 +0200

    Merge conflicts

--- project/Carrot.gd
+++ project/Carrot.gd
@@ -3,7 +3,7 @@ extends Node2D
 var attached := true
 var being_pulled_for = 0.0
 onready var shape: RectangleShape2D = get_node("Area2D/CollisionShape2D").shape
-onready var rabbit = get_parent().get_node("Rabbit")
+onready var rabbit = get_parent().get_parent().get_node("Rabbit")
 var speed := 0.0
 
 # Called when the node enters the scene tree for the first time.
@@ -12,7 +12,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	if not attached:
 		if position.y + shape.extents.y < rabbit.position.y + rabbit.shape.extents.y:
 			speed += 10
