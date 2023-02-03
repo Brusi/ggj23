5e206b97518a3675383eea72ff1fbbcad15c37d9
Brusi <bbrusi@gmail.com>
2023-02-03 10:46:59 +0200

    Tidy up the carrots

---
+++ project/CarrotContainer.gd
@@ -0,0 +1,15 @@
+tool
+
+extends Node2D
+
+func _ready():
+	if get_child_count() <= 1:
+		return
+	
+	var count: = get_child_count()
+	var min_x:float = get_child(0).position.x
+	var max_x:float = get_child(count-1).position.x
+	var diff: = 42
+	
+	for i in range(get_child_count()):
+		get_child(i).position.x = min_x + i * diff
