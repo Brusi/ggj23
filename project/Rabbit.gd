9779435e96f23d091803ae4f89279d464881de85
imraf <41264171+imraf@users.noreply.github.com>
2023-02-03 11:46:51 +0200

    Adding death function to Rabbit

--- project/Rabbit.gd
+++ project/Rabbit.gd
@@ -2,28 +2,33 @@ extends Node2D
 
 export var min_x: = 980
 export var max_x: = 1920
-
 export var speed: = 200.0
+var is_dead: bool = false
 
 func _ready():
-	pass # Replace with function body.
+	pass
 
-func _physics_process(delta):
+func _physics_process(delta):	
 	var dir: = 0
 	if Input.is_action_pressed("rabbit_right"):
 		dir += 1
 	if Input.is_action_pressed("rabbit_left"):
 		dir -= 1
-	position.x += speed * delta * dir
+	if not self.is_dead:
+		position.x += speed * delta * dir
+		if dir > 0:
+			$Sprite.flip_h = false
+			$Sprite/Clone.flip_h = false
+		elif dir < 0:
+			$Sprite.flip_h = true
+			$Sprite/Clone.flip_h = true
 	
 	if position.x < min_x - 21:
 		position.x += (max_x - min_x)
 	elif position.x > max_x - 21:
 		position.x -= (max_x - min_x)
+
+func die():
+	self.is_dead = true
+	$Sprite.flip_v = true
 	
-	if dir > 0:
-		$Sprite.flip_h = false
-		$Sprite/Clone.flip_h = false
-	elif dir < 0:
-		$Sprite.flip_h = true
-		$Sprite/Clone.flip_h = true
