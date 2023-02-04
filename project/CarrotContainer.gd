3cffd9a59fbdbc30fcd7924f40585a51bb84cd63
AsafNudler <me@asafnudler.com>
2023-02-04 15:27:20 +0200

    Integrate new BG and dims

--- project/CarrotContainer.gd
+++ project/CarrotContainer.gd
@@ -9,7 +9,7 @@ func _ready():
 	var count: = get_child_count()
 	var min_x:float = get_child(0).position.x
 	var max_x:float = get_child(count-1).position.x
-	var diff: = 42
+	var diff: = 46.34
 	
 	for i in range(get_child_count()):
 		get_child(i).position.x = min_x + i * diff
